bc552506a533ba86ae6d0aedc7b680ae5c89a197
Dick Henrik Larsson <dicklarsson@gmail.com>
2021-12-25 11:29:23 +0100

    As a buyer I can post an order

--- client/App.tsx
+++ client/App.tsx
@@ -9,9 +9,7 @@ interface IProps {
   logOut: any;
   user: User;
 }
-export function App({firebase, logOut, user}: IProps) {
- 
- 
+export function App({ firebase, logOut, user }: IProps) {
   const dollarAmountPay = 60;
   const dollarAmountGet = 57;
   return (
@@ -21,11 +19,28 @@ export function App({firebase, logOut, user}: IProps) {
         dollarAmountPay={dollarAmountPay}
         dollarAmountGet={dollarAmountGet}
       />
-      <button className="button-27">Buy Ravencoin with Paypal</button>
+      <button
+        className="button-27"
+        onClick={() => {
+          alert("Buy");
+          postBuyOrder(firebase, user);
+        }}
+      >
+        Buy Ravencoin with Paypal
+      </button>
       <button className="button-27 sign-out-button" onClick={logOut}>
         Sign out
       </button>
     </div>
   );
 }
- 
+
+function postBuyOrder(firebase, user) {
+  const ref = firebase.database().ref("/orders/" + user.uid);
+ const promise =  ref.push().set({
+    givemesome: true,
+    userTime: new Date().toISOString()
+  });
+
+  console.log("BUY", promise);
+}
